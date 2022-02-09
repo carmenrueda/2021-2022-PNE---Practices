0db94e7f7fea587aa88fe484a452be311d56ee99
carmen <cm.rueda.2021@alumnos.urjc.es>
2022-02-09 18:40:11 +0100

    Session 01

--- P0/Ex3.py
+++ P0/Ex3.py
@@ -2,5 +2,13 @@ import Seq0
 
 FOLDER = "../Session-04/"
 list_genes = ["U5", "FRAT1", "ADA", "FXN", "RNU6_269P"]
+list_length = []
 for l in list_genes:
-    print(len(Seq0.seq_read_fasta(FOLDER + l + ".txt")))
+    length = (len(Seq0.seq_read_fasta(FOLDER + l + ".txt")))
+    list_length.append(length)
+
+zipped= list(zip(list_genes, list_length))
+for l in zipped:
+    print(l[0], l[1])
+
+
