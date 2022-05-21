45c3ec7305ead20ce11884da848f61955dae878a
cxrmen <cm.rueda.2021@alumnos.urjc.es>
2022-05-21 23:53:28 +0200

    P0

--- Final-Project/server2.py
+++ Final-Project/server2.py
@@ -6,10 +6,9 @@ import termcolor
 import tools
 
 PORT = 8080
-HTML = "./html/"
-VALID_ENDPOINTS = ["/", "listSpecies", "/karyotype", "/chromosomeLength", "/geneSeq", "/geneInfo", "/geneCalc", "geneList"]
-OK = 200
+VALID_ENDPOINTS = ["/", "/listSpecies", "/karyotype", "/chromosomeLength", "/geneSeq", "/geneInfo", "/geneCalc", "/geneList"]
 ERROR = 400
+OK = 200
 
 socketserver.TCPServer.allow_reuse_address = True
 
@@ -17,35 +16,35 @@ socketserver.TCPServer.allow_reuse_address = True
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        termcolor.cprint(self.requestline, 'green')
-        termcolor.cprint(self.path, 'blue')
-        url = urlparse(self.path)
 
+        termcolor.cprint(self.requestline, 'green')
+        termcolor.cprint(self.path, 'yellow')
+        url = urlparse(self.path)
         endpoint = url.path
-        print(f"Endpoint: {endpoint}")
         arg = parse_qs(url.query)
+        print(f"Endpoint: {endpoint}")
         print(f"Parameters: {arg}")
-        status = 404
-        error = False
+
+        bad_request = False
+        status = ERROR
         contents = ""
 
         if endpoint in VALID_ENDPOINTS:
-
             if endpoint == "/":
                 status = OK
-                contents = Path(HTML + "index.html").read_text()
+                contents = Path("./html/index.html").read_text()
 
             elif endpoint == "/listSpecies":
                 if len(arg) == 0:
                     status, contents = tools.list_species()
                 elif len(arg) == 1:
                     try:
-                        limit = int(arg['limit'][0])
-                        status, contents = tools.list_species(limit)
-                    except (KeyError, IndexError, ValueError):  # Exception
-                        error = True
+                        lim = int(arg['limit'][0])
+                        status, contents = tools.list_species(lim)
+                    except (KeyError, IndexError, ValueError):
+                        bad_request = True
                 else:
-                    error = True
+                    bad_request = True
 
             elif endpoint == "/karyotype":
                 if len(arg) == 1:
@@ -53,30 +52,30 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         species = int(arg['species'][0])
                         status, contents = tools.karyotype(species)
                     except (KeyError, IndexError):
-                        error = False
+                        bad_request = True
                 else:
-                    error = False
+                    bad_request = True
 
             elif endpoint == "/chromosomeLength":
                 if len(arg) == 2:
                     try:
-                        species = int(arg['species'][0])
-                        chromosomes = arg['chromosomes'][0]
-                        status, contents = tools.chrom_length(species, chromosomes)
+                        species = arg['specie'][0]
+                        chromosome = arg['chromo'][0]
+                        status, contents = tools.chromosome_length(species, chromosome)
                     except (KeyError, IndexError):
-                        error = False
+                        bad_request = True
                 else:
-                    error = False
+                    bad_request = True
 
             elif endpoint == "/geneSeq":
                 if len(arg) == 1:
                     try:
-                        gene = int(arg['gene'][0])
+                        gene = arg['gene'][0]
                         status, contents = tools.gene_seq(gene)
                     except (KeyError, IndexError):
-                        error = False
+                        bad_request = True
                 else:
-                    error = False
+                    bad_request = True
 
             elif endpoint == "/geneInfo":
                 if len(arg) == 1:
@@ -84,9 +83,9 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         gene = arg['gene'][0]
                         status, contents = tools.gene_info(gene)
                     except (KeyError, IndexError):
-                        error = False
+                        bad_request = True
                 else:
-                    error = False
+                    bad_request = True
 
             elif endpoint == "/geneCalc":
                 if len(arg) == 1:
@@ -94,28 +93,25 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         gene = arg['gene'][0]
                         status, contents = tools.gene_calc(gene)
                     except (KeyError, IndexError):
-                        error = False
+                        bad_request = True
                 else:
-                    error = False
+                    bad_request = True
 
             elif endpoint == "/geneList":
                 if len(arg) == 3:
                     try:
-                        chromo = int(arg['chromo'][0])
+                        chromosome = arg['chromo'][0]
                         start = int(arg['start'][0])
-
-                        chromo = int(arg['chromo'][0])
-                        gene = arg['gene'][0]
-                        status, contents = tools.gene_calc(gene)
-                    except (KeyError, IndexError):
-                        error = False
+                        end = int(arg['end'][0])
+                        status, contents = tools.gene_list(chromosome, start, end)
+                    except (KeyError, ValueError, IndexError):
+                        bad_request = True
                 else:
-                    error = False
+                    bad_request = True
+        else:
+            bad_request = True
 
-            else:
-                error = True
-
-        if error:
+        if bad_request:
             contents = Path("./html/error.html").read_text()
 
         self.send_response(status)
@@ -123,6 +119,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         self.send_header('Content-Length', str(len(contents.encode())))
         self.end_headers()
         self.wfile.write(contents.encode())
+
 
 handler = TestHandler
 with socketserver.TCPServer(("", PORT), handler) as httpd:
--- Final-Project/tools.py
+++ Final-Project/tools.py
@@ -1,154 +1,126 @@
 import http.client
 import json
-import jinja2
+import jinja2 as j
 from pathlib import Path
 from Seq1 import Seq
 
 
-SERVER = "http://rest.ensembl.org"
+SERVER = "rest.ensembl.org"
 HTML = "./html/"
-valid = 200
-error = 400
+OK = 200
+ERROR = 400
+
 
 def read_html_file(filename):
     contents = Path(HTML + filename).read_text()
-    contents = jinja2.Template(contents)
+    contents = j.Template(contents)
     return contents
+
+
+def error_html():
+    status = ERROR
+    contents = Path(HTML + "error.html").read_text()
+    return status, contents
+
+
+def get_response(endpoint, params):
+    url = endpoint + params
+    conn = http.client.HTTPConnection(SERVER)
+    conn.request("GET", url)
+    response = conn.getresponse()
+    status = OK
+    return response, status
+
 
 def list_species(limit=None):
     endpoint = '/info/species'
-    params = '?content-type=application/json'
-    url = endpoint + params
-
-    conn = http.client.HTTPConnection(SERVER)
-    conn.request("GET", url)
-    response = conn.getresponse()
-    status = valid
-
-    if response.status == valid:
+    arg = '?content-type=application/json'
+    response, status = get_response(endpoint, arg)
+    if response.status == OK:
         data = json.loads(response.read().decode("utf8"))
         try:
-            species = data["species"]
-            contents = read_html_file(HTML + "species.html")\
-                .render(context={
-                "total": len(species),
-                "species": species,
-                "limit": limit
-            })
+            species = data['species']
+            contents = read_html_file("species.html").render(context={"total": len(species), "species": species, "limit": limit})
         except KeyError:
-            status = error
-            contents = Path(HTML + "error.html").read_text()
-    else:
-        status = error
-        contents = Path(HTML + "error.html").read_text()
+            status, contents = error_html()
+    elif response.status == ERROR:
+            status, contents = error_html()
+    return status, contents
+
 
 def karyotype(species):
     endpoint = '/info/assembly/'
-    params = f'{species}?content-type=application/json'
-    url = endpoint + params
-
-    conn = http.client.HTTPConnection(SERVER)
-    conn.request("GET", url)
-    response = conn.getresponse()
-    status = valid
-
-    if response.status == valid:
+    arg = f'{species}?content-type=application/json'
+    response, status = get_response(endpoint, arg)
+    if response.status == OK:
         data = json.loads(response.read().decode("utf8"))
         try:
             karyotype = data["karyotype"]
-            contents = read_html_file(HTML + "karyotype.html")\
-                .render(context={
-                "karyotype": karyotype
-            })
+            contents = read_html_file(HTML + "karyotype.html").render(context={"karyotype": karyotype})
         except KeyError:
-            status = error
-            contents = Path(HTML + "error.html").read_text()
-    else:
-        status = error
-        contents = Path(HTML + "error.html").read_text()
+            status, contents = error_html()
+    elif response.status == ERROR:
+        status, contents = error_html()
+    return status, contents
 
 
-def chrom_length(species, chromosomes):
+def chrom_length(species, chromosome):
     endpoint = '/info/assembly/'
-    params = f'{species}?content-type=application/json'
-    url = endpoint + params
-
-    conn = http.client.HTTPConnection(SERVER)
-    conn.request("GET", url)
-    response = conn.getresponse()
-    status = valid
-
-    if response.status == valid:
+    arg = f'{species}?content-type=application/json'
+    response = get_response(endpoint, arg)
+    if response.status == OK:
         data = json.loads(response.read().decode("utf8"))
         try:
-            top_level_region = data["top_level_region"]
-            len = 0
-            for chromosome in top_level_region:
-                if chromosome['name'] == chromosomes:
-                    lenght = chromosomes['length']
-                    break
-            contents = read_html_file(HTML + "species.html")\
-                .render(context={
-                "length": len,
-            })
+            chromo_dict = data["top_level_region"]
+            length = 0
+            for c in chromo_dict:
+                if c['name'] == chromosome:
+                    length = chromosome['length']
+                    break #???????????????????????????????????????????????
+            contents = read_html_file(HTML + "species.html").render(context={"length": length})
         except KeyError:
-            status = error
-            contents = Path(HTML + "error.html").read_text()
-    else:
-        status = error
-        contents = Path(HTML + "error.html").read_text()
+            status, contents = error_html()
+    elif response.status == ERROR:
+        status, contents = error_html()
+    return status, contents
+
 
 def get_id(gene):
     endpoint = '/homology/symbol/human/'
-    params = f'{gene}?content-type=application/json'
-    url = endpoint + params
-
-    conn = http.client.HTTPConnection(SERVER)
-    conn.request("GET", url)
-    response = conn.getresponse()
-
-    ok = True
+    arg = f'{gene}?content-type=application/json'
+    response = get_response(endpoint, arg)
+    valid = True
     id = None
-
-    if response.status == valid:
+    if response.status == OK:
         data = json.loads(response.read().decode("utf8"))
         try:
-            bases = data['data'][0]['id']
+            id = data['data'][0]['id']
         except(KeyError, IndexError):
-            ok = False
+            valid = False
     else:
-        ok = False
+        valid = False
+    return valid, id
 
 
 def gene_seq(gene):
-    ok, id = get_id(gene)
-    if ok:
+    valid, id = get_id(gene)
+    if valid:
         endpoint = '/sequence/id'
         params = f'{id}?content-type=application/json'
-        url = endpoint + params
-
-        conn = http.client.HTTPConnection(SERVER)
-        conn.request("GET", url)
-        response = conn.getresponse()
-        status = valid
+        response = get_response(endpoint, params)
+        status = None
+        contents = ""
 
         if response.status == valid:
             data = json.loads(response.read().decode("utf8"))
             try:
                 bases = data['seq']
-                contents = read_html_file(HTML + "gene_seq.html") \
-                    .render(context={
-                    "gene": gene,
-                    "bases": bases
-                })
+                contents = read_html_file(HTML + "gene_seq.html").render(context={"gene": gene, "bases": bases})
             except KeyError:
-                status = error
-                contents = Path(HTML + "error.html").read_text()
-        else:
-            status = error
-            contents = Path(HTML + "error.html").read_text()
-
-        return status, contents
+                status, contents = error_html()
+        elif response.status == 400:
+            status, contents = error_html()
+    return status, contents
 
 
 def gene_info(gene):
@@ -156,12 +128,9 @@ def gene_info(gene):
     if ok:
         endpoint = '/overlap/id'
         params = f'{id}?feature=gene;content-type=application/json'
-        url = endpoint + params
-
-        conn = http.client.HTTPConnection(SERVER)
-        conn.request("GET", url)
-        response = conn.getresponse()
-        status = valid
+        response = get_response(endpoint, params)
+        status = None
+        contents = ""
 
         if response.status == valid:
             data = json.loads(response.read().decode("utf8"))
@@ -170,51 +139,32 @@ def gene_info(gene):
                 end = data[0]['end']
                 length = end - start
                 chrom_name = data[0]['assembly_name']
-                contents = read_html_file(HTML_FOLDER + "gene_info.html") \
-                    .render(context={
-                    "gene": gene,
-                    "start": start,
-                    "end": end,
-                    "id": id,
-                    "lenght": length,
-                    "chrom_name": chrom_name
-                })
+                contents = read_html_file(HTML + "gene_info.html").render(context={"gene": gene, "start": start, "end": end, "id": id, "lenght": length, "chrom_name": chrom_name})
             except KeyError:
-                status = error
-                contents = Path(HTML_FOLDER + "error.html").read_text()
-        else:
-            status = error
-            contents = Path(HTML_FOLDER + "error.html").read_text()
-
-        return status, contents
+                status, contents = error_html()
+        elif response.status == 400:
+            status, contents = error_html()
+    return status, contents
 
 
 def gene_calc(gene, seq):
     endpoint = '/info/assembly/'
     params = f'{gene}?content-type=application/json'
-    url = endpoint + params
-
-    conn = http.client.HTTPConnection(SERVER)
-    conn.request("GET", url)
-    response = conn.getresponse()
-    status = valid
+    response = get_response(endpoint, params)
+    status = None
+    contents = ""
 
     if response.status == valid:
         data = json.loads(response.read().decode("utf8"))
         try:
             bases = data['seq']
             seq = Seq(bases)
-            contents = read_html_file(HTML_FOLDER + "gene_calc.html") \
-                .render(context={
-                "gene": gene,
-                "seq": seq,
-            })
+            contents = read_html_file(HTML + "gene_calc.html").render(context={"gene": gene, "seq": seq})
         except KeyError:
-            status = error
-            contents = Path(HTML_FOLDER + "error.html").read_text()
-    else:
-        status = error
-        contents = Path(HTML_FOLDER + "error.html").read_text()
+            status, contents = error_html()
+    elif response.status == 400:
+        status, contents = error_html()
+    return status, contents
 
 
 
